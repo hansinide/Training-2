a7c48d9478bfab5ea0489ec844b6c74e2c9986b3
Hansini vithanage <hansinivithanage2@gmail.com>
2020-01-20 09:48:21 +0530

    first

---
+++ src/main/java/com/hansini/exercise/App.java
@@ -0,0 +1,12 @@
+package com.hansini.exercise;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Class1 o1 = new Class1();
+    	o1.print();
+    	
+    	
+    }
+}
---
+++ src/main/java/com/hansini/exercise/Class1.java
@@ -0,0 +1,10 @@
+package com.hansini.exercise;
+
+public class Class1 {
+
+	
+	public void print () {
+		
+		System.out.println("Hello world");
+	}
+}
